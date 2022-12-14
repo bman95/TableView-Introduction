0b9b14684d37ea2e2455c04950e0065696983f64
Brendon Crowe <brendon.m.crowe@gmail.com>
2022-12-14 15:18:32 -0500

    Initial Commit

---
+++ TableView-Introduction/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TableView-Introduction
+//
+//  Created by Brendon Crowe on 12/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // testing Data
+        print("There are \(Country.countries.count) countries in our struct")
+        
+        
+        
+    }
+
+
+}
+
